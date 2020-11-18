b1b7610413a47addf6d15f7b1ce4bfd6a414ef6c
Fahid <fahid@Fahids-MacBook-Pro.local>
2020-11-19 02:49:19 +0500

    merging

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MultiSelect from './components/MultiSelect/MultiSelect';
+
+function App() {
+  return (
+    <div className="App">
+      <MultiSelect />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+export const isEmpty = (obj) => {
+    if(obj)
+      return Object.keys(obj).length === 0;
+    
+    return true;
+  }
+  
